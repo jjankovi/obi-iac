ccdae82d4e58325352b7544de888ee542e20dffd
jankovj <jaroslav.jankovic@softec.sk>
2024-08-17 14:57:25 +0200

    try s3 backend

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.20.1"
-    }
-  }
-
-}
-
-provider "aws" {
-  region  = "eu-central-1"
-}
-
 resource "aws_instance" "example" {
   ami  = "ami-00060fac2f8c42d30"
   instance_type = "t3.micro"
