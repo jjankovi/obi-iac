84d0aca46472da23c39100ae25d9bb111d738ceb
jankovj <jaroslav.jankovic@softec.sk>
2024-09-19 16:39:49 +0200

    Last Checkov bug fixed

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "ec2_instance" {
   ami           = "ami-00060fac2f8c42d30"
   instance_type = "t3.micro"
   monitoring = true
   ebs_optimized = true
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
   metadata_options {
     http_endpoint = "disabled"
   }
@@ -12,4 +13,24 @@ resource "aws_instance" "example" {
   tags = {
     Environment  = var.environment
   }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
 }
