9ffac57938ba8caaa05b7cf4fa3c40cd76d1c83a
jankovj <jaroslav.jankovic@softec.sk>
2024-09-19 16:25:55 +0200

    Remove EKS for now

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
 resource "aws_instance" "example" {
-  ami  = "ami-00060fac2f8c42d30"
+  ami           = "ami-00060fac2f8c42d30"
   instance_type = "t3.micro"
+  tags = {
+    Environment  = var.environment
+  }
 }
