b63edd255c11f4c57c381ca7413ebb1b0ab94859
jankovj <jaroslav.jankovic@softec.sk>
2024-09-19 16:31:05 +0200

    Some Checkov bugs fixed

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 resource "aws_instance" "example" {
   ami           = "ami-00060fac2f8c42d30"
   instance_type = "t3.micro"
+  monitoring = true
+  ebs_optimized = true
+  metadata_options {
+    http_endpoint = "disabled"
+  }
+  root_block_device {
+    encrypted     = true
+  }
   tags = {
     Environment  = var.environment
   }
