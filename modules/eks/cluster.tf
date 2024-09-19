49f8bb0720f9888293f9d8dc9361e17d43b42408
jankovj <jaroslav.jankovic@softec.sk>
2024-09-19 21:41:05 +0200

    Last private subnet

--- modules/eks/cluster.tf
+++ modules/eks/cluster.tf
@@ -9,7 +9,7 @@
 #}
 
 locals {
-  subnet_ids = ["subnet-05817cc08c4c95490", "subnet-0c23826ed22f9bec4"]
+  subnet_ids = ["subnet-05817cc08c4c95490", "subnet-0c23826ed22f9bec4", "subnet-0446f595633fdd966"]
 }
 
 resource "aws_eks_cluster" "eks_cluster" {
